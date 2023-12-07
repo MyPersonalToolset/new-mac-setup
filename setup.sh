d711c966e9a580221318f7e1d3cfe50718fcc353
ziqi.wang <ziqi.wang@thoughtworks.com>
2023-12-07 15:39:58 +0800

    chore: update

--- setup.sh
+++ setup.sh
@@ -1,35 +1,46 @@
+#  install oh-my-zsh
+$ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# brew install formulae
+# utils
 brew install ack
-brew install gh
-brew install kube-ps1
-brew install minikube
-brew install azure-cli
-brew install git
-brew install kubectx
-brew install gradle
-brew install kubernetes-cli
-brew install readline
-brew install haskell-stack
-brew install node
-brew install helm
-brew install nvm
-brew install yarn
-brew install maven  
-brew install docker
+brew install --cask scroll-reverser
 
-# brew install cask
+# app cli
+brew install git
+brew install gh
+brew install azure-cli
+brew install gradle
+
+# k8s
+brew install docker
+brew install kube-ps1
+brew install kubectx
+brew install minikube
+brew install kubernetes-cli
+brew install helm
+brew install --cask rancher
+
+# node
+brew install nvm
+brew install npm
+brew install pnpm
+brew install yarn
+
+# java
+brew install maven  
+brew install jabba 
+
+# IDE & editor
 brew install --cask visual-studio-code
 brew install --cask intellij-idea
-brew install --cask postman
-brew install --cask wechat
-brew install --cask hstracker
-brew install --cask scroll-reverser
-brew install --cask xmind-zen
 brew install --cask typora
-brew install --cask zoom
+brew install --cask xmind-zen
 
-#  install oh-my-zsh
-$ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# api client
+brew install websocat
+brew install --cask postman
+
+
