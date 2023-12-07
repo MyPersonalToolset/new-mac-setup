a40af50ec07a13dd311d20fd82a391aca4574b68
ziqi.wang <ziqi.wang@thoughtworks.com>
2023-12-07 15:32:27 +0800

    chore: init

---
+++ setup.sh
@@ -0,0 +1,35 @@
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# brew install formulae
+brew install ack
+brew install gh
+brew install kube-ps1
+brew install minikube
+brew install azure-cli
+brew install git
+brew install kubectx
+brew install gradle
+brew install kubernetes-cli
+brew install readline
+brew install haskell-stack
+brew install node
+brew install helm
+brew install nvm
+brew install yarn
+brew install maven  
+brew install docker
+
+# brew install cask
+brew install --cask visual-studio-code
+brew install --cask intellij-idea
+brew install --cask postman
+brew install --cask wechat
+brew install --cask hstracker
+brew install --cask scroll-reverser
+brew install --cask xmind-zen
+brew install --cask typora
+brew install --cask zoom
+
+#  install oh-my-zsh
+$ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
